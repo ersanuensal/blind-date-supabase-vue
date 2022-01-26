c3eacef5751cedb8d25600665d435cb2ab7d7539
ersanuensal <76909364+ersanuensal@users.noreply.github.com>
2022-01-26 03:12:37 +0100

    2

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-npm run build --base=./
+npm run build --base=/blind-date-supabase-vue/
 
 cd dist
 
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:ersanuensal/blind-date-supabase-vue.git master:deploy
+git push -f git@github.com:ersanuensal/blind-date-supabase-vue.git master:live
 
 cd -
