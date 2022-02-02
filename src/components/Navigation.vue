c3d58eb40f834eb417ef58f6aa2f113f11a6c3c0
ersanuensal <ersan.uensal@stud.fra-uas.de>
2022-02-02 19:31:23 +0100

    push to prod

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -36,4 +36,12 @@ export default {
     return { logout, user };
   },
 };
+
+// Check that service workers are supported
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js');
+  });
+}
 </script>
